eed939cc34832834810e6de155fd452cabcaf38c
siciliano-diag <siciliano@diag.uniroma1.it>
2022-03-01 11:09:52 +0100

    reqs

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+install_requires = []
+if os.path.isfile("requirements.txt"):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+setup(name="yourpackage", , [...])
+
 setup(
     name='basic_ML',
     packages=['basic_ML'],
     description='Basic Machine LEarning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.0.1',
+    install_requires=install_requires,
     url='https://github.com/siciliano-diag/basic_ML.git',
     author='siciliano-diag',
     author_email='siciliano@diag.uniroma1.it',
     keywords=['pip','MachineLearning']
     )
+
+
+
+
+#license='MIT',
+#project_urls = {"Bug Tracker": "https://github.com/mike-huls/toolbox/issues"},
+
