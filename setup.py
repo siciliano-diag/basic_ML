96312709112a1a8cf358cd7c5e57697555a10626
siciliano-diag <siciliano@diag.uniroma1.it>
2022-03-01 09:59:22 +0100

    package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='basic_ML',
+    packages=['basic_ML'],
+    description='Basic Machine LEarning',
+    version='0.0.1',
+    url='https://github.com/siciliano-diag/basic_ML.git',
+    author='siciliano-diag',
+    author_email='siciliano@diag.uniroma1.it',
+    keywords=['pip','MachineLearning']
+    )
