5f5afc3bfa0409558bb994dfa69f1d6add989f13
siciliano-diag <siciliano@diag.uniroma1.it>
2022-05-23 11:21:18 +0200

    change

--- basic_ML/pipeline_methods.py
+++ basic_ML/pipeline_methods.py
@@ -12,7 +12,7 @@ def load_packages(self, *package_names): #IMPORT MODEL-SPECIFIC SCRIPTS
 				print("PACKAGE IMPORTED CORRECTLY")
 
 				print("IMPORTING SUBMODULES")
-				load_packages(getattr(self,package_name), *[".".join([package_name,x]) for x in os.listdir(package_name)])
+				load_packages(getattr(self,package_name), *[os.path.join(package_name,x) for x in os.listdir(package_name)])
 			else:
 				if package_name[-3:] == ".py":
 					setattr(self,package_name,__import__(package_name[:-3]))
