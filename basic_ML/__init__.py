fa16810c5d37cc16fdaaf649766c81245699e86b
siciliano-diag <siciliano@diag.uniroma1.it>
2022-06-04 14:15:19 +0200

    change

--- basic_ML/__init__.py
+++ basic_ML/__init__.py
@@ -1,11 +1,11 @@
-from . import model_utils
+from . import model_utils_app
 #from . import pipeline
 from ._version import __version__
 
 import os
 print(os.getcwd())
 
-__all__ = ["model_utils",
+__all__ = ["model_utils_app",
     #'pipeline',
     '__version__']
 
