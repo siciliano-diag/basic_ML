481f3ec800aab7912e4a5e9eb13da655df8c4b8e
siciliano-diag <siciliano@diag.uniroma1.it>
2022-06-04 12:23:01 +0200

    change

--- basic_ML/__init__.py
+++ basic_ML/__init__.py
@@ -1,10 +1,9 @@
 from . import pipeline
-from . import model_utils
+#from . import model_utils
 from ._version import __version__
 
 __all__ = [
     'pipeline',
-    'model_utils'
     '__version__']
 
 
